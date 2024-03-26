ea3af024b61bc0f6d9c4e01feebb58de04e1f350
Eunsu Park <phd.choux@gmail.com>
2024-03-26 15:33:57 +0900

    Update options.py

--- classification/options.py
+++ classification/options.py
@@ -16,10 +16,8 @@ class BaseOptions():
         self.parser.add_argument('--in_channels', type=int, default=1)
         self.parser.add_argument('--num_classes', type=int, default=2)
         self.parser.add_argument('--image_size', type=int, default=224)
-        self.parser.add_argument("--data_root", type=str, default="/content/drive/MyDrive/CBNU/Classification/Dataset")
-        self.parser.add_argument("--save_root", type=str, default="/content/drive/MyDrive/CBNU/Classification/Result")
-        # self.parser.add_argument("--data_root", type=str, default="path/to/data")
-        # self.parser.add_argument("--save_root", type=str, default="path/to/save")
+        self.parser.add_argument("--data_root", type=str, default="path/to/data")
+        self.parser.add_argument("--save_root", type=str, default="path/to/save")
 
 
     def parse(self):
